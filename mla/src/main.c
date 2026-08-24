86b4bd5b77afd54611b466c23ed7926f3a9555b9
LOLRAPOSO <matheuslimaramalho888@gmail.com>
2026-08-23 21:04:18 -0300

    Feita estrutura base

---
+++ mla/src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINHAS 1024
+
+int main(argc, char *argv[]){
+    FILE *commands = NULL;
+    char line[MAX_LINHAS];
+
+    if (argc > 2){
+        printf("\nUltrapassou quantidade de argumentos.\n");
+    }
+}
